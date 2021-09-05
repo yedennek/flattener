1d96c36d165de81f86189ceb3aa66700fd7b3834
Anna Kennedy <yedennek@googlemail.com>
2021-09-05 11:38:59 +0100

    Handle single layer of nesting

--- lib/array_flattener.rb
+++ lib/array_flattener.rb
@@ -1,6 +1,18 @@
 class ArrayFlattener
 
   def self.flatten input_array
-    return []
+    return [] if input_array.empty?
+    
+    output = []
+
+    input_array.each do |element|
+      if element.kind_of?(Array)
+        output = output + element
+      else
+        output << element
+      end
+    end
+
+    output
   end
 end
--- spec/array_flattener_spec.rb
+++ spec/array_flattener_spec.rb
@@ -1,4 +1,5 @@
 require 'array_flattener'
+require 'byebug'
 
 describe ArrayFlattener do
 
@@ -7,7 +8,23 @@ describe ArrayFlattener do
       expect(ArrayFlattener.flatten([])).to eq []
     end
 
+    it "returns input if there is no nesting" do
+      input = [1,2,3,4]
+      expect(ArrayFlattener.flatten(input)).to eq input
+    end
     
+    context "when there is a single layer of nesting" do
+      it "flattens the input for non-empty arrays" do
+        input = [1,[2,3,4]]
 
+        expect(ArrayFlattener.flatten(input)).to eq [1,2,3,4]
+      end
+
+      it "flattens the input for empty arrays" do
+        input = [[]]
+
+        expect(ArrayFlattener.flatten(input)).to eq []
+      end
+    end
   end
 end
